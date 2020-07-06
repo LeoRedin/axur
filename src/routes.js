d63145a8f35787624d5cfd16901671d83bd59b10
Leonardo Redin Pinto Figueiró <leonardo@hygiabank.com>
2020-07-05 22:20:07 -0300

    initial summary

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import PostsScreen from 'views/Posts'
+import Summary from 'views/Summary'
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={PostsScreen} />
+      <Route exact path="/summary" component={Summary} />
+      <Redirect from="*" to="/" />
     </Switch>
   )
 }
